0bbdea9974a0903389248507747c0b6eee5e3632
Jonghyeok Park <jonghyeok_park@korea.ac.kr>
2025-02-20 16:38:13 +0900

    mop code

--- include/s3_wrapper.h
+++
@@ -1,8 +0,0 @@
-#ifndef S3_WRAPPER_H
-#define S3_WRAPPER_H
-
-void upload_to_s3(const char* bucket_name, const char* key, const char* file_path);
-void s3_wrapper_example(const char* msg);
-
-
-#endif // S3_WRAPPER_H
--- sqlite_src/src/s3_wrapper.h
+++
@@ -1,9 +0,0 @@
-#ifndef _SQLITE_S3_WRAPPER_H_
-#define _SQLITE_S3_WRAPPER_H_
-
-#include "libs3.h"
-
-void put_object();
-void test();
-
-#endif
